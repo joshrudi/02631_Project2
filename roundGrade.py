34dd27be65bf41d0ca5f5230f8e2afcf013e55db
Jakub Reha <44469049+JakubReha@users.noreply.github.com>
2018-12-04 14:22:28 +0100

    more comments

--- roundGrade.py
+++ roundGrade.py
@@ -2,20 +2,20 @@
 
 import numpy as np
 
-
+#function takes in an array of grades and returns an array with those grades rounded to the scale grades
 def roundGrade(grades):
-
+    #scale grades
     scale = np.array([-3, 0, 2, 4, 7, 10, 12])
     # initializing an empty array which we will append the rounded grades to
     gradesRounded = ([])
-    # we will go through the scale array and compare the input grade to the scale grades
+    # we will go through the scale array (while loop) for each of the input grades (for loop) and determine which one is the closest
     for i in range(np.size(grades)):
 
         k = 0
         # if we already assign a rounded value to a grade, we end the while loop
         notAssigned = True
         while notAssigned:
-        #first we treat special cases
+            #first we treat special cases
             if grades[i] <= -3:
 
                     gradesRounded = np.append(gradesRounded, -3)
@@ -24,7 +24,7 @@ def roundGrade(grades):
 
                     gradesRounded = np.append(gradesRounded, 12)
                     notAssigned = False
-            #we determine between which two scale grades the input grade lies        
+            #determines between which two scale grades the input grade lies        
             elif grades[i] < scale[k]:
                 #then compute which scale grade is the closer one
                 if abs(grades[i] - scale[k-1]) < abs(grades[i] - scale[k]):
